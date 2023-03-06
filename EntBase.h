e2fe72d5575bd99093d7558b53fd824043fc3cca
benfo <ben.foxcroft@gmail.com>
2023-03-06 17:29:33 +0000

    Creating the repo

---
+++ EntBase.h
@@ -0,0 +1,24 @@
+#ifndef ENTBASE_H
+#define ENTBASE_H
+
+#include <string>
+
+class EntBase
+{
+private:
+    std::string m_name{};
+    int m_rating{};
+    std::string m_type{};
+    bool completed{};
+
+public:
+    //getName
+
+    //getRating
+
+    //getType
+
+    //isCompleted
+};
+
+#endif
