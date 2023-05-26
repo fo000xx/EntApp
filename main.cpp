0452f0a885b106f7f4cd9f8a31f6d02c3aed6b3d
benfo <ben.foxcroft@gmail.com>
2023-05-26 15:19:19 +0100

    v0.01

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 
 int main() 
 {
+    //create a Book class
+    //has a struct{ series, genre, rating, read }
+    //save entries into a map, key is the author: book?
+    //function to print read entries, unread entries
+    //function to update information within the struct
+    //function to add new book
+    
     return 0;
 }
